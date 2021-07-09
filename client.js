9c4031be1ec8abf45d1be8b78adb2f779442b253
Ewan Vogelgesang <Ewanv2006@gmail.com>
2021-07-08 21:24:45 -0500

    Three.js
    Added style.css and working Three.js

--- client.js
+++ client.js
@@ -3,6 +3,16 @@ var serverIP = "localhost";
 
 const socket = new io('ws://'+serverIP+':16666');
 
+//scene
+var gameScene = new THREE.Scene();
+const camera = new THREE.OrthographicCamera(window.innerWidth/-2, window.innerWidth/2, window.innerHeight/2, window.innerHeight/-2, 1, 1000);
+camera.position.set(0, 0, -1);
+gameScene.add(camera);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
 socket.on("connect", () => {
     console.log("Connected to server.");
+    renderer.render(gameScene, camera);
 });
