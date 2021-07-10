18c3e7c3f67c06f1e0b48eb6a9867f3f5ccca5d5
Ewan Vogelgesang <Ewanv2006@gmail.com>
2021-07-10 10:44:01 -0500

    Failed server connection (For Nifley to fix)
    Tried to get data sent to the server.

--- client.js
+++ client.js
@@ -9,10 +9,39 @@ const camera = new THREE.OrthographicCamera(window.innerWidth/-2, window.innerWi
 camera.position.set(0, 0, -1);
 gameScene.add(camera);
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
+
+var player = new THREE.Object3D();
+gameScene.add(player);
+
+function resizeRenderer() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    //$(".game").css({"width": window.innerWidth, "height": window.innerHeight});
+}
+
+resizeRenderer();
+window.addEventListener('resize', resizeRenderer, false);
+
+function animate() {
+    requestAnimationFrame(animate);
+    renderer.render(gameScene, camera);
+}
 
 socket.on("connect", () => {
     console.log("Connected to server.");
-    renderer.render(gameScene, camera);
+    sendUpdateToServer();
+    animate();
 });
+
+function sendUpdateToServer() {
+    let packet = {
+        "type": "playerupdate",
+        "data": {
+        "position": "noodles", //[player.position.x, player.position.y],
+        "rotation": "toodles"//[player.rotation.z],
+        }
+    }
+    console.log(JSON.stringify(packet));
+    socket.emit("message", JSON.stringify(packet));
+    //socket.emit("playerupdate", JSON.stringify(data));
+  }
