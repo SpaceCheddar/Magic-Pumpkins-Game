6917aeb99ece7a105719628f9b21fe51e663ab0d
Ewan Vogelgesang <Ewanv2006@gmail.com>
2021-07-10 10:44:01 -0500

    Failed server data send (For Nifley to fix)
    Tried to get data sent to the server.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Magic Pumpkins
+## and other spooky wizarding tricks
  Code for a little halloween sandbox survival game.
--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 const http = require("http").createServer();
-var clients = {};
+var clients = [];
 var players = {};
 
 const io = require('socket.io')(http, {
     cors: { orgin: "*" }
 });
 
-io.on("connection", () => {
-    console.log("A user has connected.");
+io.on("connection", (socket) => {
+    clients.push(socket.id);
+    console.log("User "+socket.id+" has connected.");
+    console.log(clients);
+
+    players[socket.id] = {"position": [0, 0], "rotation": 0}
+
+    //io.on("playerupdate", (data) => {
+    //    parsed = JSON.parse(data);
+
+    //    players[socket.id] = parsed;
+    //    console.log(players[socket.id]);
+    //});
+
+    io.on("message", (packet) => {
+        parsed = JSON.parse(packet);
+        if(packet.type == "playerupdate") {
+            players[socket.id] = parsed.data;
+            
+        }
+        console.log(players);
+    });
 });
 
 http.listen(16666, () => console.log("Listening on port 16666."));
