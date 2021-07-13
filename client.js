78df00622dd7692f0fff68d1f1ba1f1ffbf2b327
Ewan Vogelgesang <Ewanv2006@gmail.com>
2021-07-13 09:46:13 -0500

    Working Players
    Switched to canvas.js, player connection, disconnection and update all working

--- client.js
+++ client.js
@@ -1,47 +1,126 @@
-//main menu etc
-var serverIP = "localhost";
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
-const socket = new io('ws://'+serverIP+':16666');
+fullscreen();
 
-//scene
-var gameScene = new THREE.Scene();
-const camera = new THREE.OrthographicCamera(window.innerWidth/-2, window.innerWidth/2, window.innerHeight/2, window.innerHeight/-2, 1, 1000);
-camera.position.set(0, 0, -1);
-gameScene.add(camera);
-const renderer = new THREE.WebGLRenderer();
-document.body.appendChild(renderer.domElement);
-
-var player = new THREE.Object3D();
-gameScene.add(player);
-
-function resizeRenderer() {
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    //$(".game").css({"width": window.innerWidth, "height": window.innerHeight});
+function fullscreen() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 }
+window.addEventListener("resize", fullscreen, false);
 
-resizeRenderer();
-window.addEventListener('resize', resizeRenderer, false);
+var player = {"username": "Treelr", "character": "wizard", "position": {"x": 400, "y": 100}, "direction": "right"};
+//main menu etc
+
+//game
+var serverIP = "localhost";
+const socket = new io('ws://'+serverIP+':16666');
+var players = [];
+var keysPressed = {"w": false, "a": false, "s": false, "d": false};
 
 function animate() {
     requestAnimationFrame(animate);
-    renderer.render(gameScene, camera);
+
+    var moved = false;
+    if(keysPressed.w) {player.position.y -= 5; moved = true;}
+    if(keysPressed.s) {player.position.y += 5; moved = true;}
+    if(keysPressed.a) {player.position.x -= 5; player.direction = "left"; moved = true;}
+    if(keysPressed.d) {player.position.x += 5; player.direction = "right"; moved = true;}
+
+    if(moved) sendPlayerUpdate();
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    ctx.font = '10px Arial';
+    ctx.textAlign = "center";
+    ctx.textBaseLine = "top";
+    for(var i = 0; i < players.length; i++) {
+        ctx.fillStyle = "black";
+        ctx.fillText(players[i].username, players[i].updatable.position.x, players[i].updatable.position.y - 20);
+        if(players[i].character == "wizard") ctx.fillStyle = "blue";
+        ctx.fillRect(players[i].updatable.position.x - 5, players[i].updatable.position.y - 10, 10, 10);
+    }
 }
 
+document.addEventListener("keydown", (event) => {
+    var code = event.code;
+    if(code == "KeyW") keysPressed.w = true;
+    if(code == "KeyA") keysPressed.a = true;
+    if(code == "KeyS") keysPressed.s = true;
+    if(code == "KeyD") keysPressed.d = true;
+});
+
+document.addEventListener("keyup", (event) => {
+    var code = event.code;
+    if(code == "KeyW") keysPressed.w = false;
+    if(code == "KeyA") keysPressed.a = false;
+    if(code == "KeyS") keysPressed.s = false;
+    if(code == "KeyD") keysPressed.d = false;
+});
+
+//server connection
 socket.on("connect", () => {
     console.log("Connected to server.");
-    sendUpdateToServer();
+    sendConnectionData();
+});
+
+socket.on("pcr", (data) => { //player connect respond
+    var parsed = JSON.parse(data);
+    var newPlayer;
+    for(var i = 0; i < parsed.length; i++) {
+        var notFound = true;
+        for(var j = 0; j < players.length; j++) {
+            if(parsed[i].client == players[j].client) {
+                notFound = false;
+            }
+        }
+        if(notFound) {
+            players.push(parsed[i]);
+            console.log(parsed[i].username + " has joined.");
+        }
+    }
     animate();
 });
 
-function sendUpdateToServer() {
-    let packet = {
-        "type": "playerupdate",
-        "data": {
-        "position": "noodles", //[player.position.x, player.position.y],
-        "rotation": "toodles"//[player.rotation.z],
+socket.on("pdc", (data) => { //player disconnected
+    var parsed = JSON.parse(data);
+
+    for(var i = 0; i < players.length; i++) {
+        if(players[i].client == parsed) {
+            console.log(players[i].username + " has left.");
+            players.splice(i, 1);
+            break;
         }
     }
-    console.log(JSON.stringify(packet));
-    socket.emit("message", JSON.stringify(packet));
-    //socket.emit("playerupdate", JSON.stringify(data));
+});
+
+socket.on("pupd", (data) => { //player update
+    parsed = JSON.parse(data);
+    for(var i = 0; i < players.length; i++) {
+        if(players[i].client == parsed.client) {
+            players[i].updatable = parsed.updatable;
+            break;
+        }
+    }
+});
+
+function sendConnectionData() {
+    let data = {
+        "username": player.username,
+        "character": player.character,
+        "updatable": {
+            "position": player.position,
+            "direction": player.direction
+        }
+    }
+    socket.emit("pcon", JSON.stringify(data)); //player connect
+}
+
+function sendPlayerUpdate() {
+    let data = {
+        "position": player.position,
+        "direction": player.direction 
+    }
+    socket.emit("pupd", JSON.stringify(data)); //player update
   }
--- server.js
+++ server.js
@@ -6,27 +6,40 @@ const io = require('socket.io')(http, {
     cors: { orgin: "*" }
 });
 
+function sendData(type, data, sender) {
+    stringifiedData = JSON.stringify(data);
+    sender.emit(type, stringifiedData);
+}
+
 io.on("connection", (socket) => {
     clients.push(socket.id);
     console.log("User "+socket.id+" has connected.");
-    console.log(clients);
 
-    players[socket.id] = {"position": [0, 0], "rotation": 0}
+    players[socket.id] = {"client": socket.id, "character": "wizard", "username": undefined, "updatable": {}};
 
-    //io.on("playerupdate", (data) => {
-    //    parsed = JSON.parse(data);
-
-    //    players[socket.id] = parsed;
-    //    console.log(players[socket.id]);
-    //});
-
-    io.on("message", (packet) => {
-        parsed = JSON.parse(packet);
-        if(packet.type == "playerupdate") {
-            players[socket.id] = parsed.data;
-            
+    socket.on("pcon", (data) => { //player connect
+        var parsed = JSON.parse(data);
+        players[socket.id].username = parsed.username;
+        players[socket.id].character = parsed.character;
+        players[socket.id].updatable = parsed.updatable;
+        var playerList = [];
+        for(var player in players) {
+            playerList.push(players[player]);
         }
-        console.log(players);
+        sendData("pcr", playerList, io); //player connect respond
+    })
+
+    socket.on("pupd", (data) => { //player update
+        var parsed = JSON.parse(data);
+        players[socket.id].updatable = parsed;
+        sendData("pupd", players[socket.id], io);
+    });
+
+    socket.on("disconnect", () => {
+        console.log(socket.id+" has disconnected.");
+        clients.splice(clients.indexOf(socket.id));
+        delete players[socket.id];
+        sendData("pdc", socket.id, io); //player disconnect
     });
 });
 
